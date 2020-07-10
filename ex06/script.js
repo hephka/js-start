4dc2b77dfed6eca454da2135ca72e9728eef96c8
Paulina Hetman <info@pehaa.com>
2020-07-10 06:51:20 +0200

    Update script.js

--- ex06/script.js
+++ ex06/script.js
@@ -3,13 +3,7 @@ const question = "De quelle année date ECMA6 (ECMAScript-2015) ?"
 let answer = prompt(question)
 // utilise console.log pour vérifier le type de answer
 
-// convertis answer en type number
-
-/* Après la conversion les valeur numérique deviennent les nombres, par exemple '12' devient 12
-  par contre les saisis non-numérique sont transformées en NaN ('haha' -> NaN)
-*/
-
-// On peut utiliser la fonction isNaN(answer) pour vérifier si la saisie est numérique
+// On peut utiliser la fonction isNaN(answer) ou isFinite(number) pour vérifier si la saisie est numérique
 
 // affiche un de messages suivants selon le cas :
 // Vous n'avez pas saisi de valeur numérique correcte
